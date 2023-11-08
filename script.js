8b10240d2e3fd1a34bd217091d3e8302fe3d3cc7
Martin-Rojas <rojas.martin25@yahoo.com>
2023-11-07 17:19:28 -0800

    basic rock paper scissors done

--- script.js
+++ script.js
@@ -6,16 +6,48 @@ const getComputerChoice = () => {
   return choices[randomNum];
 };
 
-const playerSelection = "rock";
-
 function playRound(playerSelection, getComputerChoice) {
   if (playerSelection == "rock" && getComputerChoice == "paper") {
-    return `You lose, paper beats rock`;
+    return 2;
   } else if (playerSelection == "rock" && getComputerChoice == "scissors") {
-    return `You win, rock beats scissors`;
+    return 1;
+  } else if (playerSelection == "scissors" && getComputerChoice == "rock") {
+    return 2;
+  } else if (playerSelection == "scissors" && getComputerChoice == "paper") {
+    return 1;
+  } else if (playerSelection == "paper" && getComputerChoice == "rock") {
+    return 1;
+  } else if (playerSelection == "paper" && getComputerChoice == "scissors") {
+    return 2;
   } else {
     return `its a tie!`;
   }
 }
 
-console.log(playRound(playerSelection, getComputerChoice()));
+let playerWinnigNumber = 0;
+let computerWinnigNumber = 0;
+
+for (let games = 0; games < 5; games++) {
+  const playerSelection = prompt(`rock, paper, scissors go! : `);
+  let result = playRound(playerSelection, getComputerChoice());
+  
+  if (result === 1) {
+    playerWinnigNumber += 1;
+  } else if (result === 2) {
+    computerWinnigNumber += 1;
+  } else {
+    console.log(`tie`);
+  }
+}
+
+if (playerWinnigNumber > computerWinnigNumber) {
+  console.log(`You win!!`);
+} else if (playerWinnigNumber < computerWinnigNumber) {
+  console.log(`You lose `);
+} else {
+  console.log(`its a tie`);
+}
+
+console.log(
+  `player point ${playerWinnigNumber} computer players: ${computerWinnigNumber}`
+);
