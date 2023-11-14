9a641dccb121db0b2d91f259011376085e64e49e
Martin-Rojas <rojas.martin25@yahoo.com>
2023-11-13 19:17:23 -0800

    Add functionality to btn of rock

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const rockElement = document.querySelector("#rock");
 const paperElement = document.querySelector("#paper");
 const scissorsElement = document.querySelector("#scissors");
+const scoreBoardElement = document.querySelector("#score-board");
+const playerScoreUi = document.querySelector("#player-score");
+const computerScoreUi = document.querySelector("#computer-score");
+const winnerUi = document.querySelector("#winner");
+
+let playerWinnigNumber = 0;
+let computerWinnigNumber = 0;
+let gameRound = 0;
 
 const getComputerChoice = () => {
   const choices = ["rock", "paper", "scissors"];
@@ -24,29 +32,63 @@ function playRound(playerSelection, getComputerChoice) {
   } else if (playerSelection == "paper" && getComputerChoice == "scissors") {
     return 2;
   } else {
-    return `its a tie!`;
+    return 0;
   }
 }
 
-let playerWinnigNumber = 0;
-let computerWinnigNumber = 0;
+const addScore = (result) => {
+  if (result === 1) {
+    playerWinnigNumber += 1;
+    playerScoreUi.textContent = `Player Score: ${playerWinnigNumber}`;
+  } else if (result === 2) {
+    computerWinnigNumber += 1;
+    computerScoreUi.textContent = `Computer Score: ${computerWinnigNumber}`;
+  }
+};
 
-// for (let games = 0; games < 5; games++) {
-//   const playerSelection = prompt(`rock, paper, scissors go! : `);
-//   let result = playRound(playerSelection, getComputerChoice());
+const getWinner = (playerWinnigNumber, computerWinnigNumber) => {
+  if (playerWinnigNumber > computerWinnigNumber) {
+    return `You win!!`;
+  } else if (playerWinnigNumber < computerWinnigNumber) {
+    return `You lose `;
+  } else {
+    return `its a tie`;
+  }
+};
 
-//   if (result === 1) {
-//     playerWinnigNumber += 1;
-//   } else if (result === 2) {
-//     computerWinnigNumber += 1;
-//   } else {
-//     console.log(`tie`);
-//   }
-// }
+scoreBoardElement.textContent = `Game Round: 0`;
+playerScoreUi.textContent = `Player Score: ${playerWinnigNumber}`;
+computerScoreUi.textContent = `Computer Score: ${computerWinnigNumber}`;
 
 rockElement.addEventListener("click", (event) => {
   const playerSelection = event.target.alt;
-  console.log(playRound(playerSelection, getComputerChoice()));
+  const computerSelection = getComputerChoice();
+
+  console.log("who won: " + playRound(playerSelection, computerSelection));
+  console.log(`mychoice ${playerSelection}  comp: ${computerSelection}`);
+
+  const result = playRound(playerSelection, computerSelection);
+
+  //addScore(result);
+  if (result === 1) {
+    playerWinnigNumber += 1;
+    playerScoreUi.textContent = `Player Score: ${playerWinnigNumber}`;
+  } else if (result === 2) {
+    computerWinnigNumber += 1;
+    computerScoreUi.textContent = `Computer Score: ${computerWinnigNumber}`;
+  }
+
+  gameRound += 1;
+
+  scoreBoardElement.textContent = `Game Round: ${gameRound}`;
+
+  if (gameRound >= 5) {
+    winnerUi.textContent = getWinner(playerWinnigNumber, computerWinnigNumber);
+    gameRound = 0;
+    playerWinnigNumber = 0;
+    computerWinnigNumber = 0;
+    rockElement.disabled = true;
+  }
 });
 
 paperElement.addEventListener("click", (event) => {
@@ -59,22 +101,3 @@ scissorsElement.addEventListener("click", (event) => {
   console.log(playRound(playerSelection, getComputerChoice()));
 });
 
-
-
-
-
-
-
-
-
-if (playerWinnigNumber > computerWinnigNumber) {
-  console.log(`You win!!`);
-} else if (playerWinnigNumber < computerWinnigNumber) {
-  console.log(`You lose `);
-} else {
-  console.log(`its a tie`);
-}
-
-console.log(
-  `player point ${playerWinnigNumber} computer players: ${computerWinnigNumber}`
-);
